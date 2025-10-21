337030cbb1fc03e1952d75b0dd5f35b42e55aa64
Mahdi <mramym68@gmail.com>
2025-10-21 17:43:08 +0330

    change todos page ui and fix some bug

--- src/Components/TodoBox/TodoBox.jsx
+++ src/Components/TodoBox/TodoBox.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import { FaRegTrashCan } from "react-icons/fa6";
+import { GiConfirmed, GiCancel } from "react-icons/gi";
 
 
-export default function TodoBox({ id, title , deleteTodo}) {
+export default function TodoBox({ id, title, isComplete, deleteTodo, completeTodo }) {
 
     return (
-        <div className='lg:w-[49%] sm:w-full h-[45px] bg-white dark:bg-neutral-300 border border-neutral-500 rounded-[8px] px-4 flex justify-between items-center'>
-            <p>{title}</p>
-            <FaRegTrashCan className='text-black hover:text-red-600 text-[18px] transition-colors cursor-pointer' onClick={() => {
-                deleteTodo(id)
-            }} />
+        <div className={`w-full h-[45px] ${isComplete ? "bg-green-200" : "bg-[#cde8fc]"}  border border-neutral-500 rounded-[8px] px-4 flex justify-between items-center`}>
+            {isComplete ? <p className='text-neutral-600'><s>{title}</s></p> : <p>{title}</p>}
+            <div className='flex items-center gap-5'>
+                <FaRegTrashCan className='text-black hover:text-red-600 text-[27px] transition-colors cursor-pointer' onClick={() => {
+                    deleteTodo(id)
+                }} />
+                <button onClick={() => {
+                    completeTodo(id)
+                }}>
+                    {isComplete ? (
+                        <GiCancel className='text-black hover:text-red-600 text-[27px] transition-colors cursor-pointer' />
+                    ) : (
+                        <GiConfirmed className='text-black hover:text-green-500 text-[27px] transition-colors cursor-pointer' />
+                    )}
+                </button>
+            </div>
         </div>
     )
 }
--- src/Pages/Todos/Todos.jsx
+++ src/Pages/Todos/Todos.jsx
@@ -1,15 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import TodoBox from '../../Components/TodoBox/TodoBox'
 import Swal from 'sweetalert2'
-import { useNavigate } from "react-router-dom";
+import PageTitle from '../../Components/PageTitle/PageTitle';
 
 
 export default function Todos() {
 
     const [todo, setTodo] = useState('')
     const [allTodos, setAllTodos] = useState([])
-    const navigate = useNavigate();
-
 
     const getAllTodos = () => {
         try {
@@ -21,10 +19,6 @@ export default function Todos() {
     }
 
     useEffect(() => {
-        const user = localStorage.getItem('user')
-        if (!user) {
-            navigate("/login")
-        }
         getAllTodos()
     }, [])
 
@@ -36,14 +30,16 @@ export default function Todos() {
                 text: "لطفا مقادیر را وارد کنید",
                 icon: "error",
                 confirmButtonText: "باشه",
+                confirmButtonColor: "#357ABD",
             })
         } else {
             const newTodo = {
                 id: allTodos.length + 1,
-                title: todo
+                title: todo,
+                isComplete: false
             }
 
-            const updatedTodos = [...(Array.isArray(allTodos) ? allTodos : []), newTodo]
+            const updatedTodos = [newTodo, ...(Array.isArray(allTodos) ? allTodos : [])]
             setAllTodos(updatedTodos)
             localStorage.setItem('todos', JSON.stringify(updatedTodos))
 
@@ -52,9 +48,16 @@ export default function Todos() {
                 text: "تسک با موفقیت اضافه شد",
                 icon: "success",
                 confirmButtonText: "باشه",
+                confirmButtonColor: "#357ABD",
             })
-
             setTodo('')
+        }
+    }
+
+    const addTodoWithEnter = (event) => {
+        if (event.key === 'Enter') {
+            event.preventDefault()
+            addTodo()
         }
     }
 
@@ -80,25 +83,39 @@ export default function Todos() {
                     text: " تسک مورد نظر با موفقیت حذف شد",
                     icon: "success",
                     confirmButtonText: "باشه",
+                    confirmButtonColor: "#357ABD",
                 })
             }
         })
     }
+
+    const completeTodo = (id) => {
+        const todos = allTodos.map(todo => {
+            if (todo.id === id) {
+                return { ...todo, isComplete: !todo.isComplete }
+            }
+            return todo
+        })
+        setAllTodos(todos)
+        localStorage.setItem('todos', JSON.stringify(todos))
+    }
     return (
-        <div className='flex flex-col items-center gap-y-10'>
-            <div className='flex lg:w-[55%] justify-between'>
-                <input type="text" value={todo} className='border border-neutral-500 dark:border-white lg:w-[50%] sm:w-[60%] rounded-[8px] px-2 focus:border-purple-600 outline-0 placeholder:text-neutral-500 dark:placeholder:text-white text-black dark:text-white bg-white dark:bg-neutral-600' onChange={e => setTodo(e.target.value)} placeholder='عنوان تسک جدید...' />
-                <button onClick={addTodo} className='lg:w-[150px] sm:w-[100px] h-[40px] bg-purple-600 text-white rounded-[8px] cursor-pointer hover:bg-purple-700 transition-colors'>ثبت</button>
+        <>
+            <div className='flex justify-center'>
+                <PageTitle title={"لیست تسک ها"} />
             </div>
-            <div className='lg:w-[55%] sm:w-full flex flex-col gap-y-6 px-10 '>
-                <h1 className='lg:text-3xl sm:text-2xl text-purple-600 dark:text-purple-500'>لیست تسک ها</h1>
-                <div className='flex flex-wrap gap-1.5'>
+            <div className='p-5 flex flex-col py-10 gap-y-10'>
+                <div className='flex justify-evenly'>
+                    <input onKeyDown={e => addTodoWithEnter(e)} type="text" value={todo} className='border border-neutral-500 dark:border-white lg:w-[30%] sm:w-[60%] rounded-[8px] px-2 outline-0 focus:border-[#357ABD] focus:placeholder:text-[#357ABD] bg-white' onChange={e => setTodo(e.target.value)} placeholder='عنوان تسک جدید...' />
+                    <button onClick={addTodo} className='lg:w-[150px] sm:w-[100px] h-[40px] bg-[#4A90E2] dark:bg-[#5DADEC] text-white  dark:text-[#1F2A44] dark:hover:bg-[#82C1F0] hover:bg-[#357ABD] rounded-[8px] cursor-pointer transition-colors'>ثبت</button>
+                </div>
+                <div className='flex flex-col gap-y-2 p-3 border border-[#d4e0ed] dark:border-[#3A4A6B] rounded-[8px] dark:bg-[#2D3748]'>
                     {!allTodos.length && <h2 className='text-center text-xl text-red-500 mt-3 w-full font-bold'>هیچ تسکی وجود ندارد</h2>}
                     {allTodos.map(todo => (
-                        <TodoBox key={todo.id} {...todo} deleteTodo={deleteTodo} />
+                        <TodoBox key={todo.id} {...todo} deleteTodo={deleteTodo} completeTodo={completeTodo} />
                     ))}
                 </div>
             </div>
-        </div>
+        </>
     )
 }
